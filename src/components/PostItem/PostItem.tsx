0a4887db8cea26e3d3cb5a86bedbc30055fcccec
magrega <proshanov@yandex.ru>
2023-04-27 11:11:53 +0300

    added text trunc

--- src/components/PostItem/PostItem.tsx
+++ src/components/PostItem/PostItem.tsx
@@ -10,10 +10,12 @@ interface IPostItemProps {
 
 const PostItem: FC<IPostItemProps> = ({title, body, userId}) => {
 
+    const truncate = (str: string, length: number) => str.length > length ? str.slice(0, length) + '...' : str;
+        
     return (
         <div className="postitem-card">
             <h3>{title}</h3>
-            <p className='postitem-card__text'>{body}</p>
+            <p className='postitem-card__text'>{truncate(body, 300)}</p>
             <div >
                 <a href='https://www.facebook.com/ElonMuskOfficiaI' target='_blank' rel='noreferrer' className='postitem-card__author socials-container'><span>{userId}</span></a>
             </div>
